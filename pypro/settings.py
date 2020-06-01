dbb2e94b4ec6b2ed99efb9323d1078c5f12e9b2c
Alfonso AG <alfareiza@gmail.com>
2020-06-01 19:39:54 -0300

    Configuration of Postgres in Travis

--- pypro/settings.py
+++ pypro/settings.py
@@ -78,7 +78,6 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-print(BASE_DIR)
 default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 parse_database = partial(dj_database_url.parse, conn_max_age=600)
 DATABASES = {
