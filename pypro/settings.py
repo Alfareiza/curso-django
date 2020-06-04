708ac0420331c71af1520c0755cc5981ae63b93e
Alfonso AG <alfareiza@gmail.com>
2020-06-04 19:27:39 -0300

    Instalation of Collectfast lib

--- pypro/settings.py
+++ pypro/settings.py
@@ -32,7 +32,7 @@ DEBUG = config("DEBUG", cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
-
+COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
