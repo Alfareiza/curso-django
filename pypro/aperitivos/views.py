5d40943be4fb20c9e669defe027d85302010e5f3
Alfareiza <alfareiza@gmail.com>
2020-06-18 20:28:42 -0300

    Dictionary for videos and use of slug for them

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 157077066}
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 157077066},
+        'santamarta': {'titulo': 'Time Lapse Santa Marta', 'vimeo_id': 178990967},
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
