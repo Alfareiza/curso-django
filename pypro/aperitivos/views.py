41c44c135396cc7cf22e97fa6f48d7c0b7f7cf84
Alfareiza <alfareiza@gmail.com>
2020-06-18 20:04:33 -0300

    Creation of variables in html sons sent by python

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 157077066}
+    return render(request, 'aperitivos/video.html', context={'video': video})
