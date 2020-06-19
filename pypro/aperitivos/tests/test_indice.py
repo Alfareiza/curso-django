40578c4249f5e3297057fcc3a2515663bad91d7c
Alfareiza <alfareiza@gmail.com>
2020-06-19 18:59:35 -0300

    Test of url of video

--- pypro/aperitivos/tests/test_indice.py
+++ pypro/aperitivos/tests/test_indice.py
@@ -24,5 +24,16 @@ def test_titulos_videos(resp, titulo):
     assert_contains(resp, titulo)
 
 
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'motivacao',
+        'santamarta'
+    ]
+)
+def test_titulos_videos(resp, slug):
+    video_link = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{video_link}"')
+
 # def test_conteudo_video(resp):
 #     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/157077066"')
