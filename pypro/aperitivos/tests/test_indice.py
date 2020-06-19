16a39b4569d0ea024a9b8dfc8f5875f57bb28c04
Alfareiza <alfareiza@gmail.com>
2020-06-19 18:30:36 -0300

    Creation of html for list videos

---
+++ pypro/aperitivos/tests/test_indice.py
@@ -0,0 +1,28 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:indice'))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+@pytest.mark.parametrize(
+    'titulo',
+    [
+        'Video Aperitivo: Motivação',
+        'Time Lapse Santa Marta'
+    ]
+)
+def test_titulos_videos(resp, titulo):
+    assert_contains(resp, titulo)
+
+
+# def test_conteudo_video(resp):
+#     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/157077066"')
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,7 +1,18 @@
 # Create your views here.
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 157077066},
+    {'slug': 'santamarta', 'titulo': 'Time Lapse Santa Marta', 'vimeo_id': 178990967},
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 157077066}
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
