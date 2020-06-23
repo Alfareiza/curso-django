8a66a4a250253754dcf90efb723418cb6c80da59
Alfareiza <alfareiza@gmail.com>
2020-06-23 18:25:02 -0300

    Implementation of test models

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from pypro.aperitivos.models import Video
 
@@ -16,5 +16,5 @@ def indice(request):
 
 
 def video(request, slug):
-    video = Video.objects.get(slug=slug)
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
