66e57ad6d354d5135113b9f7b58cd5ca5b9325c9
Alfareiza <alfareiza@gmail.com>
2020-06-23 19:00:58 -0300

    Implementation of mommy lib to create objects massively

--- pypro/aperitivos/tests/test_indice.py
+++ pypro/aperitivos/tests/test_indice.py
@@ -1,11 +1,18 @@
 import pytest
 from django.urls import reverse
+from model_mommy import mommy
 
+from pypro.aperitivos.models import Video
 from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
-def resp(client):
+def videos(db):
+    return mommy.make(Video, 3)
+
+
+@pytest.fixture
+def resp(client, videos):
     return client.get(reverse('aperitivos:indice'))
 
 
@@ -13,27 +20,15 @@ def test_status_code(resp):
     assert resp.status_code == 200
 
 
-@pytest.mark.parametrize(
-    'titulo',
-    [
-        'Video Aperitivo: Motivação',
-        'Time Lapse Santa Marta'
-    ]
-)
-def test_titulos_videos(resp, titulo):
-    assert_contains(resp, titulo)
+def test_titulos_videos(resp, videos):
+    for video in videos:
+        assert_contains(resp, video.titulo)
 
 
-@pytest.mark.parametrize(
-    'slug',
-    [
-        'motivacao',
-        'santamarta'
-    ]
-)
-def test_link_videos(resp, slug):
-    video_link = reverse('aperitivos:video', args=(slug,))
-    assert_contains(resp, f'href="{video_link}"')
+def test_link_videos(resp, videos):
+    for video in videos:
+        video_link = reverse('aperitivos:video', args=(video.slug,))
+        assert_contains(resp, f'href="{video_link}"')
 
 # def test_conteudo_video(resp):
 #     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/157077066"')
