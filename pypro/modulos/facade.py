1e45d33ff8c2920c2bd807cc10b9419eeb720e7b
Alfareiza <alfareiza@gmail.com>
2020-06-27 20:16:46 -0300

    Tests

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -3,9 +3,9 @@ from typing import List
 from pypro.modulos.models import Modulo
 
 
-def listar_modulos_ordenados()-> List[Modulo]:
+def listar_modulos_ordenados() -> List[Modulo]:
     """
-    Lista módulos ordenados por títulos
+    Lista módulos ordenados por títulos.
     :return:
     """
     return list(Modulo.objects.order_by('titulo').all())
