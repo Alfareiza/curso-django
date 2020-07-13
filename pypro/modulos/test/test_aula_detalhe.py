63b73e9458475f52c78d6702827a488204f6ed14
Alfareiza <alfareiza@gmail.com>
2020-07-13 09:49:58 -0300

    Testing the breadcrumb

--- pypro/modulos/test/test_aula_detalhe.py
+++ pypro/modulos/test/test_aula_detalhe.py
@@ -29,3 +29,8 @@ def test_titulo(resp, aula: Aula):
 
 def test_link_video_aula(resp, aula: Aula):
     assert_contains(resp, f'src="https://player.vimeo.com/video/{aula.vimeo_id}"')
+
+
+def test_modulo_titulo_breadcrumb(resp, modulo: Modulo):
+    assert_contains(resp, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{modulo.titulo}</a></li>')
+
